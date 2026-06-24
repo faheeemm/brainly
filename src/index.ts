c670b27acbaa51d7cfb7c5a22da0537ae7489329
faheeemm <faheemahmedhyd82@gmail.com>
2026-06-24 16:32:18 +0530

    initialized the userSchema and implemented the user signup endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db.js";
 
 const app = express();
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
   
+  // TODO1: Add Zod Validation
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // TODO2: Hash the password
+  await UserModel.create({ username, password });
+
+  res.json({ message: "User signed up!" });
 });
 
 
 app.post("/api/v1/signin", (req, res) => {
+  
+  const username = req.body.username;
+  const password = req.body.password;
   
 });
 
