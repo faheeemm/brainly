a649fa4f3ee2d826254da157f6c6a9947fae730e
faheeemm <faheemahmedhyd82@gmail.com>
2026-06-26 15:24:47 +0530

    created user signedin endpoint with jsonwebtoken along with mongoDB and postman

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { UserModel } from "./db.js";
+import { JWT_PASS } from "./config.js";
 
 const app = express();
 app.use(express.json());
@@ -16,17 +17,36 @@ app.post("/api/v1/signup", async (req, res) => {
   const password = req.body.password;
 
   // TODO2: Hash the password
-  await UserModel.create({ username, password });
-
-  res.json({ message: "User signed up!" });
+  try {
+    
+    await UserModel.create({ username, password });
+    res.json({ message: "User signed up!" });
+    
+  } catch (e) {
+    
+    res.status(411).send({ message: "User already exists!" });
+  }
 });
 
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
   
   const username = req.body.username;
   const password = req.body.password;
-  
+
+  const existingUser = await UserModel.findOne({ username, password });
+
+  if (existingUser) {
+    
+    const token = jwt.sign({
+      id: existingUser._id,
+    }, JWT_PASS);
+
+    // returns the token of a user when signedin
+    res.json({ token });
+  } else {
+    res.status(403).json({ message: "Invalid Creds!" });
+  }
 });
 
 app.post("/api/v1/content", (req, res) => {
@@ -48,3 +68,6 @@ app.post("/api/v1/brain/share", (req, res) => {
 app.get("/api/v1/brain/:shareLink", (req, res) => {
   
 });
+
+
+app.listen(3000);
