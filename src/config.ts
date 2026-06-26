ae6f5b677459287ddc3d93ed2c0dfe3acbf54169
faheeemm <faheemahmedhyd82@gmail.com>
2026-06-26 15:39:04 +0530

    created contentSchema for the contents table

--- src/config.ts
+++ src/config.ts
@@ -7,12 +7,14 @@ if(!MONGO_URL) {
   throw new Error("Mongo URL is not defined.")
 }
 
+export const mongoURL: string = MONGO_URL;
+
+
 const JWT_PASSWORD = process.env.JWT_PASSWORD;
 
 if (!JWT_PASSWORD) {
   throw new Error("JWT password is not defined.");
 }
 
-export const mongoURL: string = MONGO_URL;
 export const JWT_PASS: string = JWT_PASSWORD;
 
--- src/db.ts
+++ src/db.ts
@@ -9,10 +9,18 @@ try {
   console.log("santra cannot connect", e);
 }
 
-
 const UserSchema = new Schema({
   username: { type: String, unique: true },
   password: String
 });
 
 export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+  title: String,
+  links: String,
+  tags: [{ type: mongoose.Types.ObjectId, ref: 'Tag' }],
+  userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true }
+});
+
+export const contentModel = model("Content", ContentSchema);
